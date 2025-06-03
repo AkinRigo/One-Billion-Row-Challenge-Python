f157959d8035c561685979d77e6acfa39afe9b99
Akin Rigo <akinrigo7@gmail.com>
2025-06-03 16:16:28 -0300

    Projeto One Billion Row Challenge com pandas, dask e polars

--- src/using_polars.py
+++ src/using_polars.py
@@ -1,22 +1,22 @@
 import polars as pl
 
-# Created by Koen Vossen, 
-# Github: https://github.com/koenvo
-# Twitter/x Handle: https://twitter.com/mr_le_fox
-# https://x.com/mr_le_fox/status/1741893400947839362?s=20
 def create_polars_df():
-    pl.Config.set_streaming_chunk_size(4000000)
+    pl.Config.set_streaming_chunk_size(4_000_000)
     return (
-        
-        pl.scan_csv("data/measurements.txt", separator=";", has_header=False, new_columns=["station", "measure"], schema={"station": pl.String, "measure": pl.Float64})
-        .group_by(by="station")
+        pl.scan_csv(
+            "data/measurements.txt",
+            separator=";",
+            has_header=False,
+            schema={"station": pl.String, "measure": pl.Float64}
+        )
+        .group_by("station")
         .agg(
-            max = pl.col("measure").max(),
-            min = pl.col("measure").min(),
-            mean = pl.col("measure").mean()
+            max=pl.col("measure").max(),
+            min=pl.col("measure").min(),
+            mean=pl.col("measure").mean()
         )
         .sort("station")
-        .collect(streaming=True)
+        .collect()  # sem argumento
     )
 
 if __name__ == "__main__":
@@ -24,6 +24,5 @@ if __name__ == "__main__":
 
     start_time = time.time()
     df = create_polars_df()
-    took = time.time() - start_time
     print(df)
-    print(f"Polars Took: {took:.2f} sec")
+    print(f"Polars Took: {time.time() - start_time:.2f} sec")
